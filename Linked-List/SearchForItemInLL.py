8c2f91c3bb337609164bc158b65f22e55b6f9aed
Syed <amanxsyed@gmail.com>
2024-07-09 23:56:16 +0500

    Adding appending function in SIngle Linkedlist Class

--- Linked-List/SearchForItemInLL.py
+++ Linked-List/SearchForItemInLL.py
@@ -2,3 +2,18 @@ class Node:
     def __init__(self,data):
         self.data= data
         self.next= None
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head= None
+    
+    def append(self,data):
+        new_node = Node(data)
+        if not self.head:
+            self.head= new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node= last_node.next
+        last_node.next= new_node
+
